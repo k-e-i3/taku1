02c05527bdf97f6dcf85a57aada8528ee83803f1
k-e-i3 <143052502+k-e-i3@users.noreply.github.com>
2025-12-06 17:49:13 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // ▼ここにGoogle GeminiのAPIキーを入れてください
-    API_KEY: "AIzaSyAjxxEkBy29LJon2onzQD8RlZTgBYDlHCc",
+    API_KEY: "AIzaSyAr8fzPAD6rgthPcYETB9jKBqZAZWfPMd0",
 
     // ▼使用するAIモデルのバージョン
     // 例: "gemini-1.5-flash", "gemini-1.5-pro", "gemini-2.0-flash-exp" など
